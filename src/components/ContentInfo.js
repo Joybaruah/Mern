23ef558030810424a3369cb9892f7e41f56076f2
Joybaruah <hoyyun1@gmail.com>
2022-04-15 23:24:34 +0530

    font resizing

--- src/components/ContentInfo.js
+++ src/components/ContentInfo.js
@@ -3,11 +3,11 @@ import React from 'react'
 export default function ContentInfo({MernData}) {
   return (
   <>
-    <div className=' xl:flex xl:flex-col mt-4 xl:items-center'>  
+    <div className=' xl:flex xl:flex-col mx-1 mt-4 xl:items-center'>  
       <div className='xl:w-[50vw]' >
-        <h2 className='mx-3 my-3 font-archivo text-xl xl:text-3xl text-zinc-800'>The four key Technologies :</h2>
+        <h2 className='mx-3 xl:mt-3 font-archivo text-xl xl:text-3xl text-zinc-800'>Technologies used :</h2>
 
-                <ul className='font-workSans text-sm xl:text-base my-2 mx-5 p-2 list-disc'>
+                <ul className='font-workSans text-sm xl:text-base mx-5 p-2 list-disc'>
                   <li>
                     MongoDB - document database
                   </li>
@@ -30,15 +30,28 @@ export default function ContentInfo({MernData}) {
                 <h2 className='mx-3 font-archivo text-xl text-zinc-800 xl:text-2xl'>{CurElm.Tech}</h2>
                 </div>
                 <div>
-                    <p className='font-workSans mx-2 text-sm xl:text-base p-1 w-[95vw] xl:w-fit '>{CurElm.description}</p>
-                    <button className='mx-3 text-xs xl:text-sm underline decoration-green-600 text-green-600 rounded-md font-workSans'>
+                    <p className='font-workSans mx-2 xl:text-base p-1 w-[95vw] xl:w-fit '>{CurElm.description}</p>
+                    <button className='mx-3 text-sm xl:text-sm underline decoration-green-600 text-green-600 rounded-md font-workSans'>
                     <a href={CurElm.link} target="_blank" rel="noreferrer">Read more</a>
                     </button>
                 </div>
             </div>
         )
     })}
+          <div className='xl:w-[50vw]'>
+            <h2 className='mx-3 font-archivo text-xl text-zinc-800 xl:text-2xl'>Why MERN stack?</h2>
+              <p className='font-workSans mx-2 my-2 xl:text-base p-1 w-[95vw] xl:w-fit '>
+              There are many good reasons to use the MERN Stack. For example, it allows the creation of a 3-tier architecture that includes frontend, backend, and database using JavaScript and JSON. <br />
+              <br />
+              MongoDB, which is the base of the MERN stack, is designed to store JSON data natively. Everything in it, including CLI and query language, is built using JSON and JS. The NoSQL database management system works well with NodeJS and thus, allows manipulating, representing, and storing JSON data at every tier of the application. <br />
+              <br />
+              Express is a server-side framework that wraps HTTP requests and responses and makes mapping URLs to server-side functions easy. This perfectly complements the ReactJS framework, a front-end JS framework for developing interactive UIs in HTML while communicating with the server. <br />
+              <br />
+              As the two technologies work with JSON, data flows seamlessly, making it possible to develop fast and debug easily. To make sense of the entire system, you need to understand only one language, i.e., JavaScript and the JSON document structure.
+              </p>
+          </div>
     </div>
+    
     </>
   )
 }
--- src/components/ContentStart.js
+++ src/components/ContentStart.js
@@ -6,8 +6,8 @@ export default function ContentStart() {
 
       <div className='bg-gradient-to-br from-green-100 to-gray-100'> {/* What is MERN stack? */}
         <div className='flex flex-col mx-3 p-4 items-center '>
-          <h2 className='xl:my-6 font-archivo text-xl xl:text-4xl font-semibold text-zinc-800'>MERN stack</h2>
-          <p className='font-workSans my-4 xl:my-6 xl:w-[40vw] text-sm xl:text-base'>
+          <h2 className='xl:my-6 font-archivo text-2xl xl:text-4xl font-semibold text-zinc-800'>MERN stack</h2>
+          <p className='font-workSans my-4 xl:my-6 xl:w-[40vw]'>
             <span className='text-green-600'>MERN Stack</span> is a Javascript Stack that is used for easier and faster deployment of full-stack web applications. MERN Stack comprises of 4 technologies namely: <span className='text-green-500'>MongoDB, Express, React and Node.js</span>. It is designed to make the development process smoother and easier. <br />
             <br />
             It is one of several variations of the <span className='text-green-600'>MEAN Stack</span> (MongoDB Express Angular Node), where the traditional Angular.js frontend framework is replaced with React.js. Other variants include <span className='text-green-600'>MEVN</span> (MongoDB, Express, Vue, Node), and really any frontend JavaScript framework can work.
