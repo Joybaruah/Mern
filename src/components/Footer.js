39cd8c6291c307921587a9c3c6b231c906cec590
Joy <hoyyun1@gmail.com>
2022-04-19 10:38:35 +0530

    footer fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className=' flex bg-slate-400 bottom-0 h-6 font-workSans text-xs items-center justify-center'>
+    <footer>
+      <div className='container flex bg-slate-400  h-6 font-workSans text-xs items-center justify-center'>
       ©Joy Baruah
     </div>
+    </footer>
   )
 }
 
